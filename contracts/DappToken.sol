d639255254b035127aa99e3a836269dd8a834530
Dibyo Majumder <dibyomajumder23@gmail.com>
2018-08-25 19:37:55 +0530

    transfer func implemented

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,13 +1,43 @@
 pragma solidity ^0.4.2;
 
 contract DappToken {
-    // Constructor
-    // Set the total number of tokens
-    // Read the total number of tokens
-
-    uint public totalSupply;
+    // Add a name 
+    string public name = "DusterToken";
+    // Add a symbol
+    string public symbol = "Dust";
+    // Add a standard
+    string public standard = "DusterToken v1.0";
     
-    constructor() public {
-        totalSupply = 10000000;
+    uint public totalSupply;
+
+    mapping(address => uint) public balanceOf;
+
+    // Transfer Event
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    constructor(uint _initialSupply) public {
+        // allocate the entire supply
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+    }
+
+    // Transfer
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+    // Exception if account doesn't have enough
+        require(balanceOf[msg.sender] >= _value, "not enough balance to transfer");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+    // Transfer event
+        emit Transfer(msg.sender, _to, _value);
+
+    // Return a boolean
+        return true;
+
     }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,7 +1,28 @@
 var DappToken = artifacts.require("./DappToken");
 
 contract("DappToken", function(accounts) {
-  it("sets the total supply on deployment", function() {
+  var tokenInstance;
+
+  it("initializes the contract with correct values", function() {
+    return DappToken.deployed()
+      .then(function(instance) {
+        tokenInstance = instance;
+        return tokenInstance.name();
+      })
+      .then(function(name) {
+        assert.equal(name, "DusterToken", "correct name assigned to the token");
+        return tokenInstance.symbol();
+      })
+      .then(function(symbol) {
+        assert.equal(symbol, "Dust", "correct symbol assigned to the token");
+        return tokenInstance.standard();
+      })
+      .then(function(standard) {
+        assert.equal(standard, "DusterToken v1.0", "correct standard assigned");
+      });
+  });
+
+  it("alloccates the total supply on deployment", function() {
     return DappToken.deployed()
       .then(function(instance) {
         tokenInstance = instance;
@@ -12,6 +33,78 @@ contract("DappToken", function(accounts) {
           totalSupply.toNumber(),
           10000000,
           "sets the total supply to 10000000"
+        );
+        return tokenInstance.balanceOf(accounts[0]);
+      })
+      .then(function(adminBalance) {
+        assert.equal(
+          adminBalance.toNumber(),
+          10000000,
+          "it allocates the initial supply to the admin"
+        );
+      });
+  });
+
+  it("transfers token ownership", function() {
+    return DappToken.deployed()
+      .then(function(instance) {
+        tokenInstance = instance;
+        // Test `require` statement first by transferring something larger than the senders balance
+        return tokenInstance.transfer.call(accounts[1], 9999999999999999999999);
+      })
+      .then(assert.fail)
+      .catch(function(err) {
+        assert(
+          err.message.indexOf("revert") >= 0,
+          "error message must contain revert"
+        );
+        return tokenInstance.transfer.call(accounts[1], 2500000, {
+          from: accounts[0]
+        });
+      })
+      .then(function(success) {
+        assert.equal(success, true, "successfull transaction");
+        return tokenInstance.transfer(accounts[1], 2500000, {
+          from: accounts[0]
+        });
+      })
+      .then(function(reciept) {
+        assert.equal(reciept.logs.length, 1, "triggers one event");
+        assert.equal(
+          reciept.logs[0].event,
+          "Transfer",
+          "should be a transfer event"
+        );
+        assert.equal(
+          reciept.logs[0].args._from,
+          accounts[0],
+          "logs the account the tokens are transfered from"
+        );
+        assert.equal(
+          reciept.logs[0].args._to,
+          accounts[1],
+          "logs the account the tokens are transfered to"
+        );
+        assert.equal(
+          reciept.logs[0].args._value,
+          2500000,
+          "logs the transfered amount"
+        );
+        return tokenInstance.balanceOf(accounts[1]);
+      })
+      .then(function(balance) {
+        assert.equal(
+          balance.toNumber(),
+          2500000,
+          "adds the amount to reciever account"
+        );
+        return tokenInstance.balanceOf(accounts[0]);
+      })
+      .then(function(balance) {
+        assert.equal(
+          balance.toNumber(),
+          7500000,
+          "deducts the amount from the sending acc"
         );
       });
   });
