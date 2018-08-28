08ca9f8bc9516d2d13d16868407a68bd7b7a6736
Dibyo Majumder <dibyomajumder23@gmail.com>
2018-08-29 03:16:38 +0530

    Token sale contract initiated with smoke test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var DappToken = artifacts.require("./DappToken.sol");
+var DusterTokenSale = artifacts.require("./DusterTokenSale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(DappToken, 10000000);
+  deployer.deploy(DappToken, 10000000).then(function() {
+    // Token price is 0.001 eth
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(DusterTokenSale, DappToken.address, tokenPrice);
+  });
 };
