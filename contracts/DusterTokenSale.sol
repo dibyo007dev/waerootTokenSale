cd7aff0ab9dc7c7e1eba02149fa4fcc25500fdd0
Dibyo Majumder <dibyomajumder23@gmail.com>
2018-08-30 01:41:42 +0530

    buy tokens enabled with test

--- contracts/DusterTokenSale.sol
+++ contracts/DusterTokenSale.sol
@@ -6,6 +6,12 @@ contract DusterTokenSale {
     address admin;
     DappToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Sell(
+        address _buyer,
+        uint256 _amount
+    );
 // Constructor
     constructor(DappToken _tokenContract, uint256 _tokenPrice) public {
         // Assign an admin
@@ -16,6 +22,30 @@ contract DusterTokenSale {
 
         // Token Price
         tokenPrice = _tokenPrice;
+    }   
+
+    // Multiply func
+
+    function multiply(uint256 x, uint256 y) internal pure returns(uint z){
+        require(y == 0 || (z = x * y) / y == x);
 
     }
+
+    // Buying tokens
+    function buyTokens(uint256 _numberOfTokens) public payable {
+
+        // Require that the value is same as the tokens
+        require(msg.value == multiply(_numberOfTokens, tokenPrice), "sender not sending the correct amout of eth for the transaction");
+        // Require that the contract have enough balance
+        require(tokenContract.balanceOf(this) >= _numberOfTokens, "not enough balance");
+        // Require the transaction is successfull
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+
+        // Keep track of no. of tokens sold
+        tokensSold += _numberOfTokens;
+        // Emit a Sell event
+        emit Sell(msg.sender, _numberOfTokens);
+        
+    }
+
 }
--- test/DusterTokenSale.js
+++ test/DusterTokenSale.js
@@ -1,8 +1,14 @@
 var DusterTokenSale = artifacts.require("./DusterTokenSale");
+var DappToken = artifacts.require("./DappToken");
 
 contract("DusterTokenSale", function(accounts) {
+  var tokenInstance;
   var tokenSaleInstance;
+  var admin = accounts[0];
+  var buyer = accounts[1];
   var tokenPrice = 1000000000000000; // in wei
+  var tokensAvailable = 750000;
+  var numberOfTokens = 10;
   it("initializes the contract with correct values", function() {
     return DusterTokenSale.deployed()
       .then(function(instance) {
@@ -19,6 +25,90 @@ contract("DusterTokenSale", function(accounts) {
       })
       .then(function(price) {
         assert.equal(price, tokenPrice, "correct price given");
+      });
+  });
+
+  it("facilitates token buying", function() {
+    return DappToken.deployed()
+      .then(function(instance) {
+        // Grab the token first
+        tokenInstance = instance;
+        return DusterTokenSale.deployed();
+      })
+      .then(function(instance) {
+        // Then grab the token sale instance
+        tokenSaleInstance = instance;
+        // Provision 75% of the token to the token sale
+        return tokenInstance.transfer(
+          tokenSaleInstance.address,
+          tokensAvailable,
+          { from: admin }
+        );
+      })
+      .then(function(reciept) {
+        var value = numberOfTokens * tokenPrice;
+        return tokenSaleInstance.buyTokens(numberOfTokens, {
+          from: buyer,
+          value: value
+        });
+      })
+      .then(function(reciept) {
+        assert.equal(reciept.logs.length, 1, "triggers one event");
+        assert.equal(reciept.logs[0].event, "Sell", "correct event triggered");
+        assert.equal(
+          reciept.logs[0].args._buyer,
+          buyer,
+          "logs the acc. that purchased the token"
+        );
+        assert.equal(
+          reciept.logs[0].args._amount,
+          numberOfTokens,
+          "logs the number of tokens purchased"
+        );
+        return tokenSaleInstance.tokensSold();
+      })
+      .then(function(amount) {
+        assert.equal(
+          amount.toNumber(),
+          numberOfTokens,
+          "increments the number of tokens sold"
+        );
+        return tokenInstance.balanceOf(buyer);
+      })
+      .then(function(balance) {
+        assert.equal(
+          balance.toNumber(),
+          numberOfTokens,
+          "buyer balance updated"
+        );
+        return tokenInstance.balanceOf(tokenSaleInstance.address);
+      })
+      .then(function(balance) {
+        assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens);
+        // Try to buy tokens different from  the ether value
+        return tokenSaleInstance.buyTokens(numberOfTokens, {
+          from: buyer,
+          value: 1
+        });
+      })
+      .then(assert.fail)
+      .catch(function(err) {
+        assert(
+          err.message.indexOf("revert") >= 0,
+          "msg.value should be equal to the token in wei"
+        );
+        // Try to buy more tokens than available
+        return tokenSaleInstance.buyTokens(800000000, {
+          from: buyer,
+          value: numberOfTokens * tokenPrice
+        });
+      })
+      .then(assert.fail)
+      .catch(function(err) {
+        assert(
+          err.message.indexOf("revert") >= 0,
+          "cannot purchase more tokens than available"
+        );
       });
   });
 });
