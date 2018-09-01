a6101cc83bf3de13a1f9f703cfe31988ed9bd898
Dibyo Majumder <dibyomajumder23@gmail.com>
2018-09-01 16:50:44 +0530

    basic-ui rendering crossed

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ App = {
   web3Provider: null,
   contracts: {},
   account: "0x0",
+  loading: false,
+  tokenPrice: 1000000000000000,
+  tokensSold: 0,
+  tokensAvailable: 750000,
 
   init: function() {
     console.log("App initialized ....");
@@ -22,11 +26,11 @@ App = {
     return App.initContracts();
   },
   initContracts: function() {
-    $.getJSON("DusterTokenSale.json", function(dappTokenSale) {
-      App.contracts.DappTokenSale = TruffleContract(dappTokenSale);
-      App.contracts.DappTokenSale.setProvider(App.web3Provider);
-      App.contracts.DappTokenSale.deployed().then(function(dappTokenSale) {
-        console.log("Duster TokenSale Address", dappTokenSale.address);
+    $.getJSON("DusterTokenSale.json", function(dusterTokenSale) {
+      App.contracts.DusterTokenSale = TruffleContract(dusterTokenSale);
+      App.contracts.DusterTokenSale.setProvider(App.web3Provider);
+      App.contracts.DusterTokenSale.deployed().then(function(dusterTokenSale) {
+        console.log("Duster TokenSale Address", dusterTokenSale.address);
       });
     }).done(function() {
       $.getJSON("DappToken.json", function(dappToken) {
@@ -36,12 +40,22 @@ App = {
           console.log("Dapp Token Address", dappToken.address);
         });
       });
+      return App.render();
     });
-
-    return App.render();
   },
 
   render: function() {
+    if (App.loading) {
+      return;
+    }
+    App.loading = true;
+
+    var loader = $("#loader");
+    var content = $("#content");
+
+    loader.show();
+    content.hide();
+
     web3.eth.getCoinbase(function(err, account) {
       if (err === null) {
         App.account = account;
@@ -49,6 +63,47 @@ App = {
         $("#accountAddress").html("Your Account : " + account);
       }
     });
+
+    App.contracts.DusterTokenSale.deployed()
+      .then(function(instance) {
+        dusterTokenSaleInstance = instance;
+        return dusterTokenSaleInstance.tokenPrice();
+      })
+      .then(function(tokenPrice) {
+        App.tokenPrice = tokenPrice;
+        console.log(tokenPrice.toNumber());
+        $(".token-price").html(
+          web3.fromWei(App.tokenPrice.toNumber(), "ether")
+        );
+        return dusterTokenSaleInstance.tokensSold();
+      })
+      .then(function(tokensSold) {
+        App.tokensSold = tokensSold.toNumber();
+        $(".tokens-sold").html(App.tokensSold);
+        $(".tokens-available").html(App.tokensAvailable);
+        // Progress status check
+        // App.tokensSold = 600000;
+        // console.log(App.tokensSold);
+        // console.log(App.tokensAvailable);
+
+        var progressPercent = (App.tokensSold / App.tokensAvailable) * 100;
+        console.log(progressPercent);
+        $("#progress").css("width", progressPercent + "%");
+
+        // Load token contract
+        return App.contracts.DappToken.deployed()
+          .then(function(instance) {
+            dappTokenInstance = instance;
+            return dappTokenInstance.balanceOf(App.account);
+          })
+          .then(function(balance) {
+            $(".dapp-balance").html(balance.toNumber());
+          });
+      });
+
+    App.loading = false;
+    loader.hide();
+    content.show();
   }
 };
 
